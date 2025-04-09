9254b4414d4758f84fab1ac266c2fbf8e689dfca
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2025-04-09 18:21:01 -0400

    recipe view tweaks

--- Recipes/Recipes/RecipeList/RecipeView.swift
+++ Recipes/Recipes/RecipeList/RecipeView.swift
@@ -10,26 +10,34 @@ import SwiftUI
 struct RecipeView: View {
   @State var recipe: Recipe
   @State var image: UIImage?
-    var body: some View {
-      VStack {
-        if let image = image {
-          Image(uiImage: image)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 50, height: 50)
-        } else {
-          ProgressView()
-            .frame(width: 50, height: 50)
+  var body: some View {
+    GeometryReader { geometry in
+      HStack {
+        Spacer()
+        VStack {
+          if let image = image {
+            Image(uiImage: image)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: geometry.size.width / 2, height: geometry.size.width / 2)
+          } else {
+            ProgressView()
+              .frame(width: geometry.size.width / 2, height: geometry.size.width / 2)
+          }
+
+          Text(recipe.name)
+            .font(.title)
+          Text(recipe.cuisine)
+            .font(.title2.italic())
+
+          // Website link that actually links out
+          // Youtube link, embedded or links out
         }
-
-        Text(recipe.name)
-        Text(recipe.cuisine)
-
-        // TODO: Website link that actually links out
-        // TODO: Youtube link, embedded or links out
+        Spacer()
       }
       .task {
         image = await ImageHandler.shared.getImage(for: recipe.photoUrlLarge)
       }
     }
+  }
 }
